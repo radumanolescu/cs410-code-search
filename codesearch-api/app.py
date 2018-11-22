94e1b1212b1778ccc4bec6400a44b7d6bac4fc1f
Kevin Jing Qiu <kevin@idempotent.ca>
2018-11-22 01:12:56 -0500

    Stub api response

---
+++ codesearch-api/app.py
@@ -0,0 +1,33 @@
+import flask
+import logging
+from flask_cors import CORS
+
+logging.basicConfig(level='INFO')
+
+logger = logging.getLogger(__name__)
+
+app = flask.Flask(__name__)
+CORS(app)
+
+@app.route('/search', methods=['GET'])
+def search():
+    args = flask.request.args
+    logger.info(args)
+    return flask.jsonify([
+        {
+            'content': "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+            'matched': [
+                (15, 20),
+                (23, 26),
+            ],
+            'score': 0.75
+        },
+        {
+            'content': "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
+            'matched': [
+                (17, 22),
+                (25, 33),
+            ],
+            'score': 0.71
+        }
+    ])
