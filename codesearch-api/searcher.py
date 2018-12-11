8e229a283e96c8b07b42517c26ff85a003381f42
Radu Manolescu <radufm2@illinois.edu>
2018-12-10 21:44:52 -0500

    Add logging

--- codesearch-api/searcher.py
+++ codesearch-api/searcher.py
@@ -3,7 +3,10 @@ import time
 import metapy
 import flask
 from flask_cors import CORS
+import logging
 
+logging.basicConfig(level='INFO')
+logger = logging.getLogger(__name__)
 
 class Searcher:
     """
@@ -19,6 +22,7 @@ class Searcher:
         self.default_ranker = metapy.index.OkapiBM25()
 
     def search(self, request):
+        logger.info("request: " + request)
         return flask.jsonify([
             {
                 'content': "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
