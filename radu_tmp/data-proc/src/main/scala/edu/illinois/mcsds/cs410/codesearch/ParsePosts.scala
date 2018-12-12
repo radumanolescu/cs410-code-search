1beb251accb336c86a9d39947a96141439da56e1
Radu Manolescu <manolescu.radu@gmail.com>
2018-12-11 22:13:26 -0500

    Parse both files. Write posts to output file.

--- radu_tmp/data-proc/src/main/scala/edu/illinois/mcsds/cs410/codesearch/ParsePosts.scala
+++ radu_tmp/data-proc/src/main/scala/edu/illinois/mcsds/cs410/codesearch/ParsePosts.scala
@@ -1,32 +1,35 @@
 package edu.illinois.mcsds.cs410.codesearch
 
-import java.io.{File, FileInputStream}
+import java.io.{File, FileInputStream, PrintWriter}
 
-import javax.xml.XMLConstants
 import javax.xml.stream.XMLInputFactory
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
-//import javax.xml.stream.XMLEventReader
-
-import scala.io.Source
-import scala.xml.pull._
 
 object ParsePosts {
   System.setProperty("jdk.xml.totalEntitySizeLimit", String.valueOf(Integer.MAX_VALUE))
 
-  // "/Users/Radu Manolescu/-/Study/MCS-DS/CS-410/prj/data/math.stackexchange.com/Posts.xml" // 2372549
-  // "/Users/Radu Manolescu/-/Study/MCS-DS/CS-410/prj/data/stackoverflow.com/Posts.xml"
-  val postsFileName = "/Users/Radu Manolescu/-/Study/MCS-DS/CS-410/prj/data/math.stackexchange.com/Posts.xml"
-  //    val xml = new javax.xml.stream.XMLEventReader(Source.fromFile("test.xml"))
+  val mathFileName = "/Users/Radu Manolescu/-/Study/MCS-DS/CS-410/prj/data/math.stackexchange.com/Posts.xml" // 2372549
+  val oflwFileName = "/Users/Radu Manolescu/-/Study/MCS-DS/CS-410/prj/data/stackoverflow.com/Posts.xml"
+  val dataFileName = "/Users/Radu Manolescu/-/Study/MCS-DS/CS-410/prj/data/stackexchange.dat"
+  val MaxElem = 5000
 
   def main(args: Array[String]): Unit = {
+    implicit val pw = new PrintWriter(new File(dataFileName))
+    Seq(mathFileName, oflwFileName).map(new File(_)).foreach { xmlPosts =>
+      writePosts(xmlPosts)
+    }
+    pw.close()
+    println("Done: ")
+  }
+
+  def writePosts(xmlPosts: File)(implicit pw: PrintWriter): Unit = {
     val inputFactory = XMLInputFactory.newInstance()
-    //    inputFactory.setProperty(XMLConstants.FEATURE_SECURE_PROCESSING, false)
-    val xsr = inputFactory.createXMLStreamReader(new FileInputStream(new File(postsFileName)))
+    val xsr = inputFactory.createXMLStreamReader(new FileInputStream(xmlPosts))
 
     var numElem = 0
 
-    while (xsr.hasNext()) {
+    while (xsr.hasNext() && numElem < MaxElem) {
       xsr.next()
       if (xsr.isStartElement()) {
         val numAttr = xsr.getAttributeCount
@@ -34,22 +37,14 @@ object ParsePosts {
           i <- 0 until numAttr
         } {
           val localName = xsr.getAttributeLocalName(i)
-          //          val namespaceURI: String = xsr.getAttributeNamespace(i)
           val value = xsr.getAttributeValue(i)
           if (localName == "Body") {
             val doc: Document = Jsoup.parse(value)
-            println(value)
-            println(".......... .......... .......... .......... .......... .......... .......... .......... \n")
-            println(doc.text())
-            println("\n---------- ---------- ---------- ---------- ---------- ---------- ---------- ---------- \n")
+            pw.write(doc.text()) // Keep the parentheses else "ambiguous reference to overloaded definition"
           }
         }
         numElem += 1
-        if (numElem % 100 == 0) throw new RuntimeException
-        //        if (numElem % 10000 == 0) println(numElem)
       }
     }
-
-    println("Done: " + numElem)
   }
 }
