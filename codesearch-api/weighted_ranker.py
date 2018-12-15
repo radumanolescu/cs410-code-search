ce5af62eb9a5df58ca3f6240fffef37f735cab55
David Landrith <dkl2@illinois.edu>
2018-12-15 18:58:19 -0500

    Added Keywords for JavaScript, PHP, Perl

--- codesearch-api/weighted_ranker.py
+++ codesearch-api/weighted_ranker.py
@@ -39,7 +39,69 @@ class CodeRanker(metapy.index.RankingFunction):
             "or", "or_eq", "private", "protected", "public", "reflexpr", "register", "reinterpret_cast", "requires",
             "return", "short", "signed", "sizeof", "static", "static_assert", "static_cast", "struct", "switch",
             "synchronized", "template", "this", "thread_local", "throw", "true", "try", "typedef", "typeid", "typename",
-            "union", "unsigned", "using", "virtual", "void", "volatile", "wchar_t", "while", "xor", "xor_eq"
+            "union", "unsigned", "using", "virtual", "void", "volatile", "wchar_t", "while", "xor", "xor_eq",
+            # Python
+            "False", "class", "finally", "is", "return", "None", "continue", "for", "lambda", "try", "True", "def",
+            "from", "nonlocal", "while", "and", "del", "global", "not", "with", "as", "elif", "if", "or", "yield",
+            "assert", "else", "import", "pass", "break", "except", "in", "raise",
+            # Ruby
+            "alias", "and", "BEGIN", "begin", "break", "case", "class", "def", "defined", "do", "else", "elsif", "END",
+            "end", "ensure", "false", "for", "if", "module", "next", "nil", "not", "or", "redo", "rescue", "retry",
+            "return", "self", "super", "then", "true", "undef", "unles", "until", "when", "while", "yield", "__FILE__",
+            "__LINE__",
+            # JavaScript
+            "else", "instanceof", "super", "boolean", "enum", "int", "switch", "break", "export", "interface",
+            "synchronized", "byte", "extends", "let", "this", "case", "false", "long", "throw", "catch", "final",
+            "native", "throws", "char", "finally", "new", "transient", "class", "float", "null", "true", "const", "for",
+            "package", "try", "continue", "function", "private", "typeof", "debugger", "goto", "protected", "var",
+            "default", "if", "public", "void", "delete", "implements", "return", "volatile", "do", "import", "short",
+            "while", "double", "in", "static", "with", "alert", "frames", "outerHeight", "all", "frameRate",
+            "outerWidth", "anchor", "function", "packages", "anchors", "getClass", "pageXOffset", "area",
+            "hasOwnProperty", "pageYOffset", "Array", "hidden", "parent", "assign", "history", "parseFloat", "blur",
+            "image", "parseInt", "button", "images", "password", "checkbox", "Infinity", "pkcs11", "clearInterval",
+            "isFinite", "plugin", "clearTimeout", "isNaN", "prompt", "clientInformation", "isPrototypeOf",
+            "propertyIsEnum", "close", "java", "prototype", "closed", "JavaArray", "radio", "confirm", "JavaClass",
+            "reset", "constructor", "JavaObject", "screenX", "crypto", "JavaPackage", "screenY", "Date", "innerHeight",
+            "scroll", "decodeURI", "innerWidth", "secure", "decodeURIComponent", "layer", "select", "defaultStatus",
+            "layers", "self", "document", "length", "setInterval", "element", "link", "setTimeout", "elements",
+            "location", "status", "embed", "Math", "String", "embeds", "mimeTypes", "submit", "encodeURI", "name",
+            "taint", "encodeURIComponent", "NaN", "text", "escape", "navigate", "textarea", "eval", "navigator", "top",
+            "event", "Number", "toString", "fileUpload", "Object", "undefined", "focus", "offscreenBuffering",
+            "unescape", "form", "open", "untaint", "forms", "opener", "valueOf", "frame", "option", "window",
+            "onbeforeunload", "ondragdrop", "onkeyup", "onmouseover", "onblur", "onerror", "onload", "onmouseup",
+            "ondragdrop", "onfocus", "onmousedown", "onreset", "onclick", "onkeydown", "onmousemove", "onsubmit",
+            "oncontextmenu", "onkeypress", "onmouseout", "onunload"
+            # PHP
+            "__halt_compiler", "abstract", "and", "array", "as", "break",
+            "callable", "case", "catch", "class", "clone", "const", "continue", "declare", "default", "die", "do",
+            "echo", "else", "elseif", "empty", "enddeclare", "endfor", "endforeach", "endif", "endswitch", "endwhile",
+            "eval", "exit", "extends", "final", "finally", "for", "foreach", "function", "global", "goto", "if",
+            "implements", "include", "include_once", "instanceof", "insteadof", "interface", "isset", "list",
+            "namespace", "new", "or", "print", "private", "protected", "public", "require", "require_once", "return",
+            "static", "switch", "throw", "trait", "try", "unset", "use", "var", "while", "xor", "yield"
+             # PERL
+            "END", "length",
+            "setpgrp", "-B", "endgrent", "link", "setpriority", "-b", "endhostent", "listen", "setprotoent", "-C",
+            "endnetent", "local", "setpwent", "-c", "endprotoent", "localtime", "setservent", "-d", "endpwent", "log",
+            "setsockopt", "-e", "endservent", "lstat", "shift", "eof", "map", "shmctl", "eval", "mkdir", "shmget",
+            "exec", "msgctl", "shmread", "exists", "msgget", "shmwrite", "exit", "msgrcv", "shutdown", "fcntl",
+            "msgsnd", "sin", "fileno", "my", "sleep", "flock", "next", "socket", "fork", "not", "socketpair", "format",
+            "oct", "sort", "formline", "open", "splice", "getc", "opendir", "split", "getgrent", "ord", "sprintf",
+            "getgrgid", "our", "sqrt", "getgrnam", "pack", "srand", "gethostbyaddr", "pipe", "stat", "gethostbyname",
+            "pop", "state", "gethostent", "pos", "study", "getlogin", "print", "substr", "getnetbyaddr", "printf",
+            "symlink", "abs", "getnetbyname", "prototype", "syscall", "accept", "getnetent", "push", "sysopen", "alarm",
+            "getpeername", "quotemeta", "sysread", "atan2", "getpgrp", "rand", "sysseek", "AUTOLOAD", "getppid", "read",
+            "system", "BEGIN", "getpriority", "readdir", "syswrite", "bind", "getprotobyname", "readline", "tell",
+            "binmode", "getprotobynumber", "readlink", "telldir", "bless", "getprotoent", "readpipe", "tie", "break",
+            "getpwent", "recv", "tied", "caller", "getpwnam", "redo", "time", "chdir", "getpwuid", "ref", "times",
+            "CHECK", "getservbyname", "rename", "truncate", "chmod", "getservbyport", "require", "uc", "chomp",
+            "getservent", "reset", "ucfirst", "chop", "getsockname", "return", "umask", "chown", "getsockopt",
+            "reverse", "undef", "chr", "glob", "rewinddir", "UNITCHECK", "chroot", "gmtime", "rindex", "unlink",
+            "close", "goto", "rmdir", "unpack", "closedir", "grep", "say", "unshift", "connect", "hex", "scalar",
+            "untie", "cos", "index", "seek", "use", "crypt", "INIT", "seekdir", "utime", "dbmclose", "int", "select",
+            "values", "dbmopen", "ioctl", "semctl", "vec", "defined", "join", "semget", "wait", "delete", "keys",
+            "semop", "waitpid", "DESTROY", "kill", "send", "wantarray", "die", "last", "setgrent", "warn", "dump", "lc",
+            "sethostent", "write", "each", "lcfirst", "setnetent"
         ]
 
         super(CodeRanker, self).__init__()
@@ -51,7 +113,7 @@ class CodeRanker(metapy.index.RankingFunction):
 
         for word in self.keywords:
             if word in line:
-                line += (' ' + word) * weight
+                line += ((' ' + word) * weight)
         query.content(line)
 
         return self.default_ranker.score(idx, query, top_k)
