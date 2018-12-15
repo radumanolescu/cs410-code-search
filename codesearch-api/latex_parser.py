6a188a6eba94f5d348885f56a9c8f04f3d61d1ab
Kevin Jing Qiu <kevin@idempotent.ca>
2018-12-15 17:24:58 -0500

    Update to the latest algorithm

--- codesearch-api/latex_parser.py
+++ codesearch-api/latex_parser.py
@@ -3,9 +3,20 @@ import re
 import hashlib
 
 
+def transform_digest(digest):
+    return ''.join([
+        ch if not str.isdigit(ch) else chr(ord(ch) + ord('K') - ord('0'))
+        for ch in digest
+    ])
+
+
 def latex_to_csx(latex_formula):
+    if latex_formula.startswith('$') and latex_formula.endswith('$'):
+        latex_formula = latex_formula[1:-1]
+    if latex_formula.startswith('$') and latex_formula.endswith('$'):
+        latex_formula = latex_formula[1:-1]
     words = [
-        '[{}]'.format(hashlib.md5(w).hexdigest()[:5]) for w in
+        'CSX_{}'.format(transform_digest(hashlib.md5(w).hexdigest()[:5].upper())) for w in
         re.split(r'\s|\\', latex_formula)
         if w
     ]
@@ -14,4 +25,4 @@ def latex_to_csx(latex_formula):
 
 if __name__ == '__main__':
     for line in sys.stdin.readlines():
-        print(latex_to_csx(line))
+        print(latex_to_csx(line.strip()))
