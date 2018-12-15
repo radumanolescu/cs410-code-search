2efccf8f50673b1336a45ce567f4f6f33397f22a
Kevin Jing Qiu <kevin@idempotent.ca>
2018-12-15 02:43:13 -0500

    Formula buttons

--- codesearch-ui/src/components/SearchBar.vue
+++ codesearch-ui/src/components/SearchBar.vue
@@ -2,13 +2,18 @@
 <div class="ui center aligned basic segment">
   <sui-form>
     <sui-form-field>
-      <sui-input placeholder="Search..." icon="search" v-on:keydown.prevent.enter="onSearch" :value="query" @input="updateQuery" />
+      <sui-input placeholder="Input search term here.  Use $$...$$ for formula search." icon="search" v-on:keydown.prevent.enter="onSearch" :value="query" @input="updateQuery" />
+      <sui-button-group class="latex-button-group">
+        <sui-button compact><mathjax formula="$$x^y$$">abc</mathjax></sui-button>
+        <sui-button compact><mathjax formula="$$\sqrt{x}$$">abc</mathjax></sui-button>
+      </sui-button-group>
     </sui-form-field>
   </sui-form>
 </div>
 </template>
 
 <script>
+import MathJax from './MathJax.vue'
 import { mapState } from 'vuex'
 
 export default {
@@ -17,6 +22,9 @@ export default {
     ...mapState({
       query: state => state.query
     })
+  },
+  components: {
+    'mathjax': MathJax
   },
   methods: {
     updateQuery (e) {
@@ -28,3 +36,10 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.latex-button-group {
+  margin: 2px;
+  padding: 5px;
+}
+</style>
