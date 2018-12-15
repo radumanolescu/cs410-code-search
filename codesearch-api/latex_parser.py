0b4964303f65e3cc7e1edc05d57ef75a14b64233
Kevin Jing Qiu <kevin@idempotent.ca>
2018-12-15 16:59:05 -0500

    latex_to_csx

--- codesearch-api/latex_parser.py
+++ codesearch-api/latex_parser.py
@@ -1,2 +1,17 @@
+import sys
+import re
+import hashlib
+
+
+def latex_to_csx(latex_formula):
+    words = [
+        '[{}]'.format(hashlib.md5(w).hexdigest()[:5]) for w in
+        re.split(r'\s|\\', latex_formula)
+        if w
+    ]
+    return ' '.join(words)
+
+
 if __name__ == '__main__':
-    print('')
+    for line in sys.stdin.readlines():
+        print(latex_to_csx(line))
